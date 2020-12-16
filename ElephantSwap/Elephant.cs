4896245d1149eac6b77519fa2f7ac9a129cadd98
lopezrobert@outlook.com <lopezrobert@outlook.com>
2020-12-16 14:10:49 -0600

    Add project files.

---
+++ ElephantSwap/Elephant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElephantSwap
+{
+    class Elephant
+    {
+        public string Name;
+        public int EarSize;
+
+        public void WhoAmI()
+        {
+            Console.WriteLine("My name is " + Name);
+            Console.WriteLine("My ears are " + EarSize + " inches tall.");
+        }
+
+        public void HearMessage(string message, Elephant whoSaidIt)
+        {
+            Console.WriteLine(Name + " heard a message");
+            Console.WriteLine(whoSaidIt.Name + " said this: " + message);
+        }
+
+        public void SpeakTo(Elephant whotoTalkTo, string message)
+        {
+            whotoTalkTo.HearMessage(message, this);
+        }
+    }
+}
---
+++ ElephantSwap/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ElephantSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephant lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+            Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+
+            Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+            do
+            {
+                Console.Write("You pressed ");
+                string strInput = Console.ReadLine();
+                if (strInput == "1")
+                {
+                    Console.WriteLine("Calling lloyd.WhoAmI()");
+                    lloyd.WhoAmI();
+                }
+                else if (strInput == "2")
+                {
+                    Console.WriteLine("Calling lucinda.WhoAmI()");
+                    lucinda.WhoAmI();
+                }
+                else if (strInput == "3")
+                {
+                    Elephant temp;
+                    temp = lucinda;
+                    lucinda = lloyd;
+                    lloyd = temp;
+                    Console.WriteLine("References have been swapped");
+                }
+                else if (strInput == "4")
+                {
+                    lloyd = lucinda;
+                    lloyd.EarSize = 4321;
+                    lloyd.WhoAmI();
+                }
+                else if (strInput == "5")
+                {
+                    lucinda.SpeakTo(lloyd, "Hi, Lloyd");
+                }
+                else return;
+                Console.WriteLine();
+
+            } while (true);
+        }
+    }
+}
